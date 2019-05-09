3509e081c2103c69102b844cc02b4314d59e68a0
root <changhong.hu@qq.com>
2019-05-09 22:57:25 +0800

    init

---
+++ mqtt.cpp
@@ -0,0 +1,44 @@
+
+
+#include "mqtt.h"
+
+void MqttController::test()
+{
+}
+
+bool MqttController::protocol_handler(task_t *co, char *buf, size_t nbyte)
+{
+    int fd = co->fd;
+    int ret = read(fd, buf, nbyte);
+    if (ret > 0)
+    {
+        char a = buf[0];
+        bool retain = (a & 1) > 0;
+        int qosLevel = (a & 0x06) >> 1;
+        bool dupFlags = (a & 0x08) > 0;
+        char msgType = (a >> 4) & 0x0F;
+
+        printf("retain: %d, qosLevel: %d, dupFlag: %d, msgType: %d\n", retain, qosLevel, dupFlags, msgType);
+
+        if (msgType == 0x01)
+        {
+            char ack[4];
+            ack[0] = 0x02 << 4;
+            ack[1] = 0x02;
+            ack[2] = 0x00;
+            ack[3] = 0x00;
+            ret = write(fd, ack, sizeof(ack));
+        }
+        else if (msgType == 0x0C)
+        {
+            co->keep_alive = time(NULL);
+            char ack[2];
+            ack[0] = 0x0D << 4;
+            ack[1] = 0x00;
+            ret = write(fd, ack, sizeof(ack));
+        }
+    }
+
+    printf("write ret: %d, errno: %d\n", ret, errno);
+    return ret > 0 || (-1 == ret && EAGAIN == errno);
+}
---
+++ mqtt.h
@@ -0,0 +1,38 @@
+
+
+#ifndef __V_MQTT_H__
+#define __V_MQTT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <stack>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+#include "libco/co_routine.h"
+
+struct task_t
+{
+    stCoRoutine_t *co;
+    int fd;
+    time_t keep_alive;
+};
+
+class MqttController
+{
+
+  public:
+    void test();
+    bool protocol_handler(task_t *co, char *buf, size_t nbyte);
+};
+
+#endif // header
